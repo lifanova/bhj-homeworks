3d77e99c876f8f81ab9264c581a5e6768e7e67a7
vlifanova <vlifanova2@alfabank.ru>
2021-09-05 22:01:27 +0300

    Task 5.1 & 5.3

---
+++ document-structure/cart/task.js
@@ -0,0 +1,72 @@
+// Контролы уменьшения / увеличения кол-ва товара в корзине
+const controlsDec = Array.from(document.querySelectorAll(".product__quantity-control_dec"));
+const controlsInc = Array.from(document.querySelectorAll(".product__quantity-control_inc"));
+// Кнопки добавления товара в корзину
+const addButtons = Array.from(document.querySelectorAll(".product__add"));
+// Корзина
+const basketElement = document.querySelector(".cart__products");
+
+controlsDec.forEach(element => {
+    element.addEventListener("click", processDec);
+});
+
+controlsInc.forEach(element => {
+    element.addEventListener("click", processInc);
+});
+
+
+addButtons.forEach(element => {
+    element.addEventListener("click", addToBasket);
+});
+
+function processDec(event) {
+    const control = event.target;
+    const quantityItem = control.closest(".product__quantity-controls")
+        .querySelector(".product__quantity-value");
+
+    // Уменьшаем счетчик
+    let value = quantityItem.textContent * 1;
+    if (value >= 1) {
+        quantityItem.textContent = value - 1;
+    }
+}
+
+function processInc(event) {
+    const control = event.target;
+    const quantityItem = control.closest(".product__quantity-controls")
+        .querySelector(".product__quantity-value");
+
+    // Увеличиваем счетчик
+    let value = quantityItem.textContent * 1;
+    quantityItem.textContent = value + 1;
+
+
+}
+
+function addToBasket(event) {
+    const button = event.target;
+    const productItem = button.closest(".product");
+    let id = productItem.dataset.id;
+    let quantity = productItem.querySelector(".product__quantity-value").textContent * 1;
+    let img = productItem.querySelector(".product__image").getAttribute("src");
+
+    // Проверяем, не добавлен ли уже этот продукт в корзину
+    let addedProducts = Array.from(basketElement.querySelectorAll(".cart__product"));
+    let index = addedProducts.findIndex(element => element.dataset.id === id);
+
+    if(index) {
+        // просто увеличиваем кол-во единиц продукта
+        
+    } else {
+        basketElement.innerHTML += `
+        <div class="cart__product" data-id="${id}">
+            <img class="cart__product-image" src="${img}">
+            <div class="cart__product-count">${quantity}</div>        
+        </div>`;
+    }
+
+   
+}
+
+
+
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,18 +1,32 @@
 const links = Array.from(document.querySelectorAll('.has-tooltip'));
 
-links.forEach(element => element.addEventListener('click', processClick));
+links.forEach(element => {
+    let tooltipText = element.getAttribute("title");
+    element.insertAdjacentHTML("afterEnd",
+        `<div class = 'tooltip'> ${tooltipText} </div>`);
+});
+
+const tooltips = Array.from(document.querySelectorAll(".tooltip"));
+
+links.forEach(element => element.addEventListener("click", processClick));
 
 function processClick(event) {
     const link = event.target;
-    const position = link.dataset.position;
-    console.log("[]");
 
     event.preventDefault();
 
-    let text = link.getAttribute("title");
-    link.insertAdjacentHTML("afterEnd", `<div class = 'tooltip'> ${text} </div>`);
+    let tooltip = link.nextElementSibling;
+    if (tooltip.classList.contains("tooltip_active")) {
+        tooltip.classList.remove("tooltip_active");
+    } else {
+        tooltips.forEach(element => element.classList.remove("tooltip_active"));
 
-    const tooltips = Array.from(document.querySelectorAll('.tooltip'));
+        tooltip.classList.add("tooltip_active");
 
-    
+        let topValue = link.getBoundingClientRect().bottom;
+        let leftValue = link.getBoundingClientRect().left;
+        tooltip.setAttribute("style", `top: ${topValue}px; left: ${leftValue}px`);
+    }
+
+
 }
